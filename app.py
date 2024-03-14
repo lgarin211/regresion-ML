be114119c02368b7d2dc7b87879db7ec8dc6eab0
Agustinus <57866141+lgarin211@users.noreply.github.com>
2024-03-14 09:53:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def hasil(result):
     return render_template('result.html', result=result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
